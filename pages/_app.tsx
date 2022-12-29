08a378d97b9532ef81788f2c5b464b4e5600eafb
tykatyk <tykatyk@gmail.com>
2022-12-29 17:30:00 +0200

    Redraw theme to be dark

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,19 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline>
+        <Component {...pageProps} />
+      </CssBaseline>
+    </ThemeProvider>
+  );
 }
