854f3adc88a14d121e0136541f580ce359d69c63
tykatyk <tykatyku@gmail.com>
2023-01-30 16:54:31 +0200

    Refactor to use prepared h component

--- components/ContactUs.tsx
+++ components/ContactUs.tsx
@@ -1,27 +1,14 @@
-import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import ContactUsForm from "./ContactUsForm";
+import H2 from "./H2";
 
 export default function ContactUs() {
   return (
-    <section>
-      <div style={{ padding: "2rem 0 4rem 0", background: "#392b5a" }}>
-        <Typography
-          align="center"
-          variant="h2"
-          sx={{
-            fontFamily: "Russo One, sans-serif",
-            fontSize: "2.5rem",
-            marginBottom: "2rem",
-            fontWeight: 700,
-          }}
-        >
-          Залишились питання?
-        </Typography>
-        <Container maxWidth={"sm"}>
-          <ContactUsForm />
-        </Container>
-      </div>
+    <section style={{ background: "#392b5a" }}>
+      <H2 text="Залишились питання?" />
+      <Container maxWidth={"sm"} style={{ paddingBottom: "4rem" }}>
+        <ContactUsForm />
+      </Container>
     </section>
   );
 }
